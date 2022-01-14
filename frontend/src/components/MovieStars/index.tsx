ae6edfdd019bd81619ab7626957143cdc6bb237e
Lucas Côrte <lucascorte@outlook.com>
2022-01-13 22:46:15 -0300

    MovieCard

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,16 @@
+import { ReactComponent as StarFull} from 'assets/img/fullStar.svg'
+import { ReactComponent as StarHalf} from 'assets/img/halfStar.svg'
+import { ReactComponent as StarEmpty} from 'assets/img/emptyStar.svg'
+import './styles.css'
+
+export function MovieStars() {
+  return (
+    <div className="dsmovie-stars-container">
+      <StarFull />
+      <StarFull />
+      <StarFull />
+      <StarHalf />
+      <StarEmpty />
+    </div>
+  );
+}
